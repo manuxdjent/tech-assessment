d8e9b207e0df800801b32f0a4697999484bbb5e9
manuxdjent <manuxdjent@gmail.com>
2024-03-25 20:22:42 +0100

    fix: use const timestamp variable instead of enviroment variable, remove unused components

--- pages/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default function Loading(): JSX.Element {
-  return <h1>LOADING!!!</h1>
-}
--- utils/ApiUtils.ts
+++ utils/ApiUtils.ts
@@ -1,9 +1,10 @@
 import md5 from 'md5'
 
 export function getBaseParams(): string {
-    const apiKeyhash = md5(`${process.env.NEXT_PUBLIC_TIMESTAMP}${process.env.NEXT_PUBLIC_PRIVATE_KEY}${process.env.NEXT_PUBLIC_PUBLIC_KEY}`)
+    const timestamp = '1'
+    const apiKeyhash = md5(`${timestamp}${process.env.NEXT_PUBLIC_PRIVATE_KEY}${process.env.NEXT_PUBLIC_PUBLIC_KEY}`)
     const urlSearchParams = new URLSearchParams({
-        ts: process.env.NEXT_PUBLIC_TIMESTAMP,
+        ts: timestamp,
         apikey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
         hash: apiKeyhash
     })
