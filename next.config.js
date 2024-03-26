35dfb36a74b5e381eb4d11d07bed9ddd7a53bb29
manuxdjent <manuxdjent@gmail.com>
2024-03-26 13:33:06 +0100

    fix: fix next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_PRIVATE_KEY: process.env.NEXT_PUBLIC_PRIVATE_KEY,
+    NEXT_PUBLIC_PUBLIC_KEY: process.env.NEXT_PUBLIC_PUBLIC_KEY
+  },
   images: {
-    env: {
-      NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-      NEXT_PUBLIC_PRIVATE_KEY: process.env.NEXT_PUBLIC_PRIVATE_KEY,
-      NEXT_PUBLIC_PUBLIC_KEY: process.env.NEXT_PUBLIC_PUBLIC_KEY
-    },
     remotePatterns: [
       {
         protocol: 'http',
